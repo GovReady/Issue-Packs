64fa611e051c3c07b7c452049ecdd5a9128d3ea3
Chris Birk <cmbirk@gmail.com>
2016-04-12 00:17:00 -0500

    require modules, remove `main` from package.json, add `bin` to package.json

--- issue-pack.js
+++ issue-pack.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+
+var argv = require('minimist')(process.argv.slice(2));
+var chalk = require('chalk');
+var Github = require('github');
+var YAML = require('yamljs');
+
+console.dir(argv);
