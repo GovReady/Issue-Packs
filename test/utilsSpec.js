b118689cc03d04e8f5963f84480956bd176bb08e
Chris Birk <cmbirk@gmail.com>
2016-04-13 00:20:07 -0500

    remove references to dotenv setups

--- test/utilsSpec.js
+++ test/utilsSpec.js
@@ -79,38 +79,6 @@ describe('Utils', function () {
     });
   });
 
-  describe('#checkEnv()', function () {
-    it('should accept a correctly formatted env', function () {
-      var env = {
-        PACK_USER: 'username',
-        PACK_PASS: 'password',
-        PACK_REPO: 'repo'
-      };
-
-      var results = utils.checkEnv(env);
-
-      expect(results).to.be.true;
-    });
-
-    it('should reject a partially complete env', function () {
-      var env = {
-        PACK_USER: 'username',
-        PACK_PASS: 'password',
-      };
-
-      var results = utils.checkEnv(env);
-
-      expect(results).to.be.false;
-    });
-
-    it('should reject an empty env', function () {
-      var env = {};
-      var results = utils.checkEnv(env);
-
-      expect(results).to.be.false;
-    });
-  });
-
   describe('#usage', function () {
     it('should return usage message', function () {
       var message = "usage: issue-pack -u username -p password -r repo pack1.yml pack2.yml ...";
