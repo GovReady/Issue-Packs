98f6f6a4dbd35e28725564347810b923b76f8d1b
David Lee <david.lee@kastling.com>
2016-12-26 13:26:59 -0500

    Adding command line argument support with parsing and validation

---
+++ src/GithubUtil.js
@@ -0,0 +1,97 @@
+"use strict";
+
+export default class GithubUtil {
+  //Set util options
+  constructor (util = null, logger = console) {
+    this.logger = logger;
+    this.util = util;
+  }
+
+  promptSchema() {
+    return {
+      properties: {
+        username: {
+          required: true
+        },
+        password: {
+          hidden: true,
+          required: true
+        },
+        repo: {
+          pattern: /^[a-zA-Z\-]+\/[a-zA-Z\-]+$/,
+          message: 'Repo must be in the form of `user/repo`',
+          required: true,
+          description: "repo (username/repo)"
+        },
+        path: {
+          required: true
+        }
+      }
+    };
+  }
+
+  parse(args) {
+    var util = this.util;
+    var options = {
+      path: []
+    };
+
+    args.forEach(function (arg) {
+      if (util.startsWith(arg, "-t=")) {
+        options.tool = arg.split('=', 2)[1];
+      } else if (util.startsWith(arg, "-u=")) {
+        options.username = arg.split('=', 2)[1];
+      } else if (util.startsWith(arg, "-p=")) {
+        options.password = arg.split('=', 2)[1];
+      } else if (util.startsWith(arg, "-r=")) {
+        options.repo = arg.split('=', 2)[1];
+      } else {
+        options.path.push(arg);
+      }
+    });
+
+    options.path = options.path.join(' ');
+
+    return options;
+  }
+
+  //Validate input arguments
+  validate(args) {
+    var error = false;
+
+    var tool = args.tool;
+    var username = args.username;
+    var password = args.password;
+    var repo = args.repo;
+    var path = args.path;
+
+    if (!tool || tool.toUpperCase() !== 'JIRA') {
+      error = true;
+    }
+
+    if (username === undefined || username === true) {
+      error = true;
+    }
+
+    if (password === undefined || password === true) {
+      error = true;
+    }
+
+    if (repo === undefined || repo === true) {
+      error = true;
+    }
+
+    if (path.length === 0) {
+      error = true;
+    }
+
+    return !error;
+  }
+
+  //Print script usage
+  usage() {
+    var message = "usage: issue-pack -t=github -u=username -p=password -r=repo pack1.yml [pack2.yml] [pack3.yml] ...";
+
+    return message;
+  }
+}
---
+++ src/JiraUtil.js
@@ -0,0 +1,106 @@
+"use strict";
+
+export default class JiraUtil {
+  //Set util options
+  constructor (util = null, logger = console) {
+    this.logger = logger;
+    this.util = util;
+  }
+
+  promptSchema() {
+    return {
+      properties: {
+        username: {
+          required: true
+        },
+        password: {
+          hidden: true,
+          required: true
+        },
+        projectKey: {
+          required: true,
+          description: "Jira Project Key (prefixes all issues in project)"
+        },
+        jiraBaseUri: {
+          required: true,
+          description: "Jira Base URI (e.g. https://jira.govready.com)"
+        },
+        path: {
+          required: true
+        }
+      }
+    };
+  }
+
+  parse(args) {
+    var util = this.util;
+    var options = {
+      path: []
+    };
+
+    args.forEach(function (arg) {
+      if (util.startsWith(arg, "-t=")) {
+        options.tool = arg.split('=', 2)[1];
+      } else if (util.startsWith(arg, "-u=")) {
+        options.username = arg.split('=', 2)[1];
+      } else if (util.startsWith(arg, "-p=")) {
+        options.password = arg.split('=', 2)[1];
+      } else if (util.startsWith(arg, "-k=")) {
+        options.projectKey = arg.split('=', 2)[1];
+      } else if (util.startsWith(arg, "-b=")) {
+        options.jiraBaseUri= arg.split('=', 2)[1];
+      } else {
+        options.path.push(arg);
+      }
+    });
+
+    options.path = options.path.join(' ');
+
+    return options;
+  }
+
+  //Validate input arguments
+  validate(args) {
+    var error = false;
+
+    var tool = args.tool;
+    var username = args.username;
+    var password = args.password;
+    var projectKey = args.projectKey;
+    var jiraBaseUri = args.jiraBaseUri;
+    var path = args.path;
+
+    if (!tool || tool.toUpperCase() !== 'JIRA') {
+      error = true;
+    }
+
+    if (username === undefined || username === true) {
+      error = true;
+    }
+
+    if (password === undefined || password === true) {
+      error = true;
+    }
+
+    if (projectKey === undefined || projectKey === true) {
+      error = true;
+    }
+
+    if (jiraBaseUri === undefined || jiraBaseUri === true) {
+      error = true;
+    }
+
+    if (path.length === 0) {
+      error = true;
+    }
+
+    return !error;
+  }
+
+  //Print script usage
+  usage() {
+    var message = "usage: issue-pack -t=jira -u=username -p=password -k=projectKey -b=baseUri pack1.yml [pack2.yml] [pack3.yml] ...";
+
+    return message;
+  }
+}
